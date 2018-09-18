af65ad875fbc16c95394c46260a7f913fc44c9dd
Bernd Zeimetz <bernd@bzed.de>
2018-09-18 19:44:02 +0200

    setup base class first

--- spec/classes/setup/dehydrated_host_spec.rb
+++ spec/classes/setup/dehydrated_host_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 describe 'dehydrated::setup::dehydrated_host' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
-
-      it { is_expected.to compile }
+    let :pre_condition do
+        'class { "dehydrated" : }'
     end
-  end
+
+    on_supported_os.each do |os, os_facts|
+        context "on #{os}" do
+            let(:facts) { os_facts }
+
+            it { is_expected.to compile }
+        end
+    end
 end
