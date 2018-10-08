b08a2236363f0ed86bfc92c4eb1dad524228004a
Bernd Zeimetz <bernd@bzed.de>
2018-10-08 17:31:28 +0200

    base64 decode the ocsp file

--- manifests/certificate/collect.pp
+++ manifests/certificate/collect.pp
@@ -60,7 +60,7 @@ define dehydrated::certificate::collect(
         has_key($config, 'ocsp') and
         $config['ocsp'] =~ Stdlib::Base64
       ) {
-        $ocsp = Binary($config['ocsp'])
+        $ocsp = Binary(base64('decode', $config['ocsp']))
       } else {
         $ocsp = undef
       }
