fea1c41ed50f9f228b00277e411419e46d15880e
Bernd Zeimetz <bernd@bzed.de>
2019-07-24 15:28:01 +0200

    Introduce a notify class

--- manifests/certificate/deploy.pp
+++ manifests/certificate/deploy.pp
@@ -17,6 +17,7 @@ define dehydrated::certificate::deploy(
   }
 
   require ::dehydrated::setup
+  include ::dehydrated::certificate::notify
 
   $dehydrated_domains = $facts['dehydrated_domains']
   $_config = $dehydrated_domains[$dn]
@@ -44,11 +45,15 @@ define dehydrated::certificate::deploy(
   }
 
   concat { $crt_full_chain :
-    mode => '0644',
+    mode   => '0644',
+    notify => Class['::dehydrated::certificate::notify'],
   }
   concat { $crt_full_chain_with_key :
     mode   => '0640',
-    notify => Dehydrated_pfx[$pfx],
+    notify => [
+      Dehydrated_pfx[$pfx],
+      Class['::dehydrated::certificate::notify'],
+    ],
   }
 
   concat::fragment { "${dn}_key" :
--- manifests/certificate/dh.pp
+++ manifests/certificate/dh.pp
@@ -29,6 +29,7 @@ define dehydrated::certificate::dh(
   dehydrated_dhparam { $dh :
     ensure => present,
     size   => $dh_param_size,
+    notify => Class['::dehydrated::certificate::notify'],
   }
 
   file { $dh:
